c700b2082834036aadffff843536ee3c1bf95979
Dina Zeng <dina.zeng22@gmail.com>
2026-06-23 23:27:40 -0700

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int, int>> hotterDays;
+        vector<int> answers(temperatures.size());
+
+        for (int i = temperatures.size() - 1; i >= 0; i--) {
+            int todaysTemp = temperatures[i];
+
+            while (!hotterDays.empty() && hotterDays.top().first <= todaysTemp) {
+                hotterDays.pop();
+            }
+
+            answers[i] = hotterDays.empty() ? 0 : hotterDays.top().second - i;
+            hotterDays.push({todaysTemp, i});
+        }
+
+        return answers;   
+    }
+};
