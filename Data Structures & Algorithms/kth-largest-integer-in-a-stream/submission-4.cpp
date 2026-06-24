45cf02c118b23c7986f9ba54896d073cc0e1fa5b
Dina Zeng <dina.zeng22@gmail.com>
2026-06-24 00:04:45 -0700

    Add: kth-largest-integer-in-a-stream - submission-4

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-4.cpp
@@ -0,0 +1,28 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> heap;
+    int max;
+
+    KthLargest(int k, vector<int>& nums) {
+        max = k;
+        for (int n : nums) {
+            heap.push(n);
+
+            if (heap.size() > max) {
+                heap.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        if (heap.size() < max) {
+            heap.push(val);
+        } else if (val > heap.top()) {
+            heap.pop();
+
+            heap.push(val);
+        }
+
+        return heap.top();
+    }
+};
