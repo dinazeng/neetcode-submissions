b18747a4c2f5ba75c7a0cfd66f62dd23f9f8619e
Dina Zeng <dina.zeng22@gmail.com>
2026-06-24 00:12:45 -0700

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> heap;
+
+        for (int n : stones) {
+            heap.push(n);
+        }
+
+        while (heap.size() > 1) {
+            int x = heap.top();
+            heap.pop();
+
+            int y = heap.top();
+            heap.pop();
+
+            if (x - y != 0) {
+                heap.push(abs(x - y));
+            }
+        }
+
+        return heap.empty() ? 0 : heap.top();
+    }
+};
