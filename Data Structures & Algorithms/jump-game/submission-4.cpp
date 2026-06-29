dbaf96ce8f24a11c9a008462dc97398194a0a815
Dina Zeng <dina.zeng22@gmail.com>
2026-06-29 04:22:56 -0700

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int size = nums.size();
+        vector<bool> jumps (size, false);
+        jumps[0] = true;
+
+        for (int i = 0; i < size; i++) {
+            if (jumps[i]) {
+                int jump = i + nums[i] + 1;
+
+                if (jump >= size) {
+                    return true;
+                }
+
+                for (int j = 0; j < jump; j++) {
+                    jumps[j] = true;
+                }
+            }
+        }
+
+
+        return jumps[size - 1];
+    }
+};
