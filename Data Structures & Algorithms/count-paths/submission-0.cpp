61086779de8802c40cb2309c0c080e9a46b0b9cb
Dina Zeng <dina.zeng22@gmail.com>
2026-06-29 03:48:50 -0700

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> grid(m, (vector<int> (n, 0)));
+        grid [0][0] = 1;
+
+        for (int i = 1; i < m; i++) {
+            grid[i][0] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            grid[0][i] = 1;
+        }
+
+        for (int r = 1; r < m; r++) {
+            for (int c = 1; c < n; c++) {
+                grid[r][c] = grid[r - 1][c] + grid[r][c - 1];
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+};
